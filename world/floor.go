a0e282aa685f5180a8fd9109e47e9a4ec5684e36
Harivansh Rathi <rathiharivansh@gmail.com>
2026-04-12 22:26:44 -0400

    feat: floor.go

--- world/floor.go
+++ world/floor.go
@@ -1,6 +1,9 @@
 package world
 
-import "sync"
+import (
+	"main/entity"
+	"sync"
+)
 
 type Floor struct {
 	Width, Height int
@@ -8,6 +11,43 @@ type Floor struct {
 	mu            sync.Mutex
 }
 
+func NewFloor(width, height int) *Floor {
+	tiles := make([][]Tile, height)
+	for y := range tiles {
+		tiles[y] = make([]Tile, width)
+		for x := range tiles[y] {
+			tiles[y][x] = Tile{Type: Wall}
+		}
+	}
+	return &Floor{
+		Width:  width,
+		Height: height,
+		Tiles:  tiles,
+	}
+}
+
 func (f *Floor) GetTile(x, y int) *Tile {
+	if x < 0 || x >= f.Width || y < 0 || y >= f.Height {
+		return nil
+	}
 	return &f.Tiles[y][x]
+}
+
+func (f *Floor) MoveEntity(e *entity.Entity, newX, newY int) bool {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	target := f.GetTile(newX, newY)
+
+	if target == nil || target.Type == Wall || target.Occupant != nil {
+		return false
+	}
+
+	f.GetTile(e.X, e.Y).Occupant = nil
+
+	target.Occupant = e
+
+	e.X, e.Y = newX, newY
+
+	return true
 }
